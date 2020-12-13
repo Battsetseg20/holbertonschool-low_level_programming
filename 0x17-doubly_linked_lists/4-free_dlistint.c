b13f835c70b6b1508d437135f3522c65d3c44530
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-13 19:56:34 +0000

    betty compliant

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * free_dlistint - frees a dlistint_t list
+ * @head: points to header node
+ * Return: void
+ */
+
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *node;
