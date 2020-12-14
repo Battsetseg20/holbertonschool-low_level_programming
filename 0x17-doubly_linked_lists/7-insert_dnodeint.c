519cf1eab5e1302ccd415511ab56f907057e401d
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-14 16:26:27 +0000

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -15,9 +15,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp_node;
 	dlistint_t *new_node;
 
-	if (h == NULL)
-		return (NULL);
-
 	if (idx == 0)
 	{
 		tmp_node = add_dnodeint(h, n);
