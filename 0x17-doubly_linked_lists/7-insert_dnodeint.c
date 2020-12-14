2e89c6f55c9d802518f69011a89f176f0d5072f8
Battsetseg20 <tsetseg.yo@gmail.com>
2020-12-14 16:24:42 +0000

    add task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -34,10 +34,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 
+	if (tmp_node->next == NULL)
+		return (add_dnodeint_end(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
 	new_node->prev = tmp_node;
 	new_node->next = tmp_node->next;
